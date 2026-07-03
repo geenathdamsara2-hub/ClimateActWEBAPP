4cab03e1a50148071b0b2c1d92e622ebade11e41
geenathdamsara2-hub <geenathdamsara2@gmail.com>
2026-07-03 06:20:16 +0530

    Fix dashboardRoutes initialization error

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -1,17 +1,8 @@
 const express = require("express");
-
 const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware");
-const adminMiddleware = require("../middleware/adminMiddleware");
 
-router.get(
-    "/",
-    authMiddleware,
-    adminMiddleware,
-    getDashboardStats
-);
 const {
-    getDashboardStats
+  getDashboardStats
 } = require("../controllers/dashboardController");
 
 router.get("/", getDashboardStats);
